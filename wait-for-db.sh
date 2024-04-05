88eadb837590230ba3c8b8e20473b21b330dd6c9
Sergioo-D <sergio.dominguez.garcia@ieselcalamot.com>
2024-04-05 15:33:02 +0200

    mas cambios 22

--- wait-for-db.sh
+++ wait-for-db.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-HOST=$(echo $1 | cut -d: -f1)
-PORT=$(echo $1 | cut -d: -f2)
-shift
-CMD="$@"
+set -e
 
-until nc -z "$HOST" "$PORT"; do
-  >&2 echo "El servicio $HOST:$PORT no está disponible aún - esperando..."
+host="$1"
+shift
+port="$1"
+shift
+cmd="$@"
+
+until mysql -h "$host" -P "$port" -u"bogdanadmin" -p"n!H7Pm9~]w2CCy8?<|yB1apDc4[>BrdkC.#|zG5EQ" -e "SHOW DATABASES;"; do
+  >&2 echo "MySQL is unavailable - sleeping"
   sleep 1
 done
 
->&2 echo "El servicio $HOST:$PORT está disponible - ejecutando comando"
-exec $CMD
+>&2 echo "MySQL is up - executing command"
+exec $cmd
