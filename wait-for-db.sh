b654c23a997bf38479c25aa9cb99c798db023e00
Sergioo-D <sergio.dominguez.garcia@ieselcalamot.com>
2024-04-05 15:21:39 +0200

    mas cambios 20

--- wait-for-db.sh
+++ wait-for-db.sh
@@ -1,30 +1,14 @@
 #!/bin/bash
 
-set -e
-
-# Parámetros de conexión a la base de datos
-host="$1"
+HOST=$(echo $1 | cut -d: -f1)
+PORT=$(echo $1 | cut -d: -f2)
 shift
-port="$1"
-shift
-cmd="$@"
-user="bogdanadmin"
-password="n!H7Pm9~]w2CCy8?<|yB1apDc4[>BrdkC.#|zG5EQ"
+CMD="$@"
 
-# Configuración de reintentos
-max_attempts=30
-attempt_num=1
-sleep_seconds=5
-
-until mysql -h "$host" -P "$port" -u"$user" -p"$password" -e "SHOW DATABASES;" > /dev/null 2>&1; do
-  >&2 echo "MySQL is unavailable - sleeping ($attempt_num/$max_attempts)"
-  sleep $sleep_seconds
-  if [ $attempt_num -eq $max_attempts ]; then
-    echo "MySQL is still unavailable after $max_attempts attempts - stopping."
-    exit 1
-  fi
-  ((attempt_num++))
+until nc -z "$HOST" "$PORT"; do
+  >&2 echo "El servicio $HOST:$PORT no está disponible aún - esperando..."
+  sleep 1
 done
 
->&2 echo "MySQL is up - executing command"
-exec $cmd
+>&2 echo "El servicio $HOST:$PORT está disponible - ejecutando comando"
+exec $CMD
