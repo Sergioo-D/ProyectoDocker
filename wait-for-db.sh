994214f3e8748f949f2565ed02100a86599d448e
Sergioo-D <sergio.dominguez.garcia@ieselcalamot.com>
2024-04-05 15:48:25 +0200

    mas cambios 25

--- wait-for-db.sh
+++ wait-for-db.sh
@@ -8,10 +8,26 @@ port="$1"
 shift
 cmd="$@"
 
-until mysql -h "$host" -P "$port" -u"bogdanadmin" -p"n!H7Pm9~]w2CCy8?<|yB1apDc4[>BrdkC.#|zG5EQ" -e "SHOW DATABASES;"; do
-  >&2 echo "MySQL is unavailable - sleeping"
+# Utiliza variables de entorno para las credenciales
+DB_USER=${DB_USER:-bogdanadmin}
+DB_PASS=${DB_PASS:-tuContraseña}
+
+# Configura un timeout para evitar intentos de conexión infinitos
+TIMEOUT=60
+SECONDS=0
+
+echo "Esperando a que MySQL en '$host:$port' esté disponible..."
+
+until mysql -h "$host" -P "$port" -u"$DB_USER" -p"$DB_PASS" -e "SHOW DATABASES;" || [ $SECONDS -ge $TIMEOUT ]; do
+  >&2 echo "MySQL aún no está disponible después de $SECONDS segundos..."
   sleep 1
+  SECONDS=$((SECONDS+1))
 done
 
->&2 echo "MySQL is up - executing command"
+if [ $SECONDS -ge $TIMEOUT ]; then
+  >&2 echo "Se alcanzó el tiempo máximo de espera de $TIMEOUT segundos para MySQL."
+  exit 1
+fi
+
+>&2 echo "MySQL está disponible después de $SECONDS segundos - ejecutando comando."
 exec $cmd
